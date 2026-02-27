5e33b418efec48b15bb28b09e03d850ed8453e9a
Mak Magz <marktmagcanta.dev@gmail.com>
2026-02-27 15:51:31 +0900

    feat: add model #5

--- backend/internal/url/model/url.go
+++ backend/internal/url/model/url.go
@@ -1,1 +1,23 @@
 package model
+
+import "time"
+
+type URL struct {
+	ID          int64     `db:"id" json:"id"`
+	OriginalURL string    `db:"original_url" json:"originalUrl"`
+	ShortCode   string    `db:"short_code" json:"shortCode"`
+	Clicks      int64     `db:"clicks" json:"clicks"`
+	CreatedAt   time.Time `db:"created_at" json:"createdAt"`
+}
+
+type CreateURLRequest struct {
+	OriginalURL string `json:"originalUrl" binding:"required"`
+}
+
+type CreateURLResponse struct {
+	ID          int64     `json:"id"`
+	OriginalURL string    `json:"originalUrl"`
+	ShortCode   string    `json:"shortCode"`
+	Clicks      int64     `json:"clicks"`
+	CreatedAt   time.Time `json:"createdAt"`
+}
