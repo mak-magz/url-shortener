3bf368b9634fbfddf7082d8477a312ed5dd19102
Mak Magz <marktmagcanta.dev@gmail.com>
2026-02-27 15:52:12 +0900

    feat: add service interface

--- backend/internal/url/service/url_service.go
+++ backend/internal/url/service/url_service.go
@@ -1,1 +1,12 @@
 package service
+
+import (
+	"context"
+
+	"github.com/mak-magz/url-shortener/internal/url/model"
+)
+
+type URLService interface {
+	CreateShortURL(ctx context.Context, req *model.CreateURLRequest) (*model.CreateURLResponse, error)
+	GetOriginalURL(ctx context.Context, shortCode string) (string, error)
+}
