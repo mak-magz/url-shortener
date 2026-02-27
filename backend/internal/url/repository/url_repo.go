df9ff935b4875d5200c57cf1168c34f13c25f94c
Mak Magz <marktmagcanta.dev@gmail.com>
2026-02-27 16:10:03 +0900

    feat: add repository interface #5

--- backend/internal/url/repository/url_repo.go
+++ backend/internal/url/repository/url_repo.go
@@ -1,1 +1,13 @@
 package url
+
+import (
+	"context"
+
+	"github.com/mak-magz/url-shortener/internal/url/model"
+)
+
+type URLRepository interface {
+	CreateShortURL(ctx context.Context, req *model.URL) error
+	GetURLByShortCode(ctx context.Context, shortCode string) (*model.URL, error)
+	IncrementClick(ctx context.Context, id int64) error
+}
