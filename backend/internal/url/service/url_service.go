3cd4c3b33ec2db1f8b17e23bc762d8b13d99a9f7
Mak Magz <marktmagcanta.dev@gmail.com>
2026-03-03 00:54:32 +0900

    feat: implement CreateShortURL #9

--- backend/internal/url/service/url_service.go
+++ backend/internal/url/service/url_service.go
@@ -2,11 +2,61 @@ package service
 
 import (
 	"context"
+	"math/rand"
+	"time"
 
 	"github.com/mak-magz/url-shortener/internal/url/model"
+	"github.com/mak-magz/url-shortener/internal/url/repository"
 )
 
-type URLService interface {
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const codeLength = 6
+
+type Service interface {
 	CreateShortURL(ctx context.Context, req *model.CreateURLRequest) (*model.CreateURLResponse, error)
 	GetOriginalURL(ctx context.Context, shortCode string) (string, error)
+}
+
+type URLService struct {
+	repo repository.URLRepository
+}
+
+func NewURLService(repo repository.URLRepository) Service {
+	return &URLService{repo: repo}
+}
+
+// CreateShortURL implements [Service].
+func (u *URLService) CreateShortURL(ctx context.Context, req *model.CreateURLRequest) (*model.CreateURLResponse, error) {
+	shortCode := generateShortCode(codeLength)
+
+	newURL := &model.URL{
+		OriginalURL: req.OriginalURL,
+		ShortCode:   shortCode,
+	}
+
+	if err := u.repo.CreateShortURL(ctx, newURL); err != nil {
+		return nil, err
+	}
+
+	return &model.CreateURLResponse{
+		ID:          newURL.ID,
+		OriginalURL: newURL.OriginalURL,
+		ShortCode:   newURL.ShortCode,
+		Clicks:      newURL.Clicks,
+		CreatedAt:   newURL.CreatedAt,
+	}, nil
+}
+
+// GetOriginalURL implements [Service].
+func (u *URLService) GetOriginalURL(ctx context.Context, shortCode string) (string, error) {
+	panic("unimplemented")
+}
+
+func generateShortCode(length int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	code := make([]byte, length)
+	for i := range code {
+		code[i] = charset[r.Intn(len(charset))]
+	}
+	return string(code)
 }
