7bbef3e63568e5d152e31016a604a2587c2afa23
Mak Magz <marktmagcanta.dev@gmail.com>
2026-03-03 00:54:32 +0900

    feat: implement CreateShortURL #9

--- backend/internal/url/repository/url_repo.go
+++ backend/internal/url/repository/url_repo.go
@@ -1,4 +1,4 @@
-package url
+package repository
 
 import (
 	"context"
---
+++ backend/internal/url/service/url_service_test.go
@@ -0,0 +1,63 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/mak-magz/url-shortener/internal/url/model"
+	"github.com/mak-magz/url-shortener/internal/url/service"
+)
+
+type MockRepo struct {
+}
+
+func (m *MockRepo) CreateShortURL(ctx context.Context, u *model.URL) error {
+
+	u.ID = 1
+	u.Clicks = 0
+	u.CreatedAt = time.Now()
+	return nil
+}
+
+func (m *MockRepo) GetURLByShortCode(ctx context.Context, shortCode string) (*model.URL, error) {
+	return nil, nil
+}
+
+func (m *MockRepo) IncrementClick(ctx context.Context, id int64) error {
+	return nil
+}
+
+func TestCreateShortURL(t *testing.T) {
+	mockRepo := &MockRepo{}
+	service := service.NewURLService(mockRepo)
+
+	req := &model.CreateURLRequest{
+		OriginalURL: "https://www.google.com",
+	}
+
+	resp, err := service.CreateShortURL(context.Background(), req)
+	if err != nil {
+		t.Errorf("Error creating short URL: %v", err)
+	}
+
+	if resp.OriginalURL != req.OriginalURL {
+		t.Errorf("Original URL does not match")
+	}
+
+	if resp.ShortCode == "" {
+		t.Errorf("Short code is empty")
+	}
+
+	if len(resp.ShortCode) != 6 {
+		t.Errorf("Short code length is not 6")
+	}
+
+	if resp.Clicks != 0 {
+		t.Errorf("Clicks should be 0")
+	}
+
+	if resp.CreatedAt.IsZero() {
+		t.Errorf("Created at is zero")
+	}
+}
