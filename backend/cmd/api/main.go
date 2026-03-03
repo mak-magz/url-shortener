7e0986f29b46e8c70886029afb127360a0f30c24
Mak Magz <marktmagcanta.dev@gmail.com>
2026-03-04 02:00:26 +0900

    feat: implement the /shorten API endpoint #11

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mak-magz/url-shortener/internal/url/handler"
+	"github.com/mak-magz/url-shortener/internal/url/repository"
+	"github.com/mak-magz/url-shortener/internal/url/service"
 	"github.com/mak-magz/url-shortener/platform/config"
 	"github.com/mak-magz/url-shortener/platform/db"
 )
@@ -27,6 +30,12 @@ func main() {
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
+
+	urlRepo := repository.NewPostgresRepository(pool)
+	urlService := service.NewURLService(urlRepo)
+	urlHandler := handler.NewURLHandler(urlService)
+
+	router.POST("/api/v1/shorten", urlHandler.CreateShortURL)
 
 	router.Run(":8080")
 }
