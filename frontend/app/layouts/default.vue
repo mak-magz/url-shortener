cf182aef7e5385c0e927fce9e66927002a6edd13
Mak Magz <marktmagcanta.dev@gmail.com>
2026-03-27 04:05:13 +0900

    feat: add header component
    #21

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,34 +1,7 @@
 <template>
   <div>
-    <UHeader>
-      <template #left>
-        <NuxtLink to="/">
-          <AppLogo class="w-auto h-6 shrink-0" />
-        </NuxtLink>
+    <AppHeader />
 
-        <TemplateMenu />
-      </template>
-
-      <template #right>
-        <UColorModeButton />
-
-        <UButton
-          to="/login"
-          label="Login"
-          color="neutral"
-          variant="ghost"
-        />
-
-        <UButton
-          to="https://github.com/nuxt-ui-templates/starter"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-          color="neutral"
-          variant="ghost"
-        />
-      </template>
-    </UHeader>
     <UMain>
       <slot />
     </UMain>
