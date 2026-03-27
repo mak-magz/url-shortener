c82e2c5d15f1f53310b0cd9b3ea03c216ff5f2dd
Mak Magz <marktmagcanta.dev@gmail.com>
2026-03-27 12:26:38 +0900

    feat: sample card layout
    #21

--- frontend/app/components/AppSampleCard.vue
+++ frontend/app/components/AppSampleCard.vue
@@ -9,24 +9,35 @@
 			</div>
 		</template>
 		<template #default>
-			<UCard variant="soft">
-				<div class="h-[200px]">
-					<div class="w-full flex flex-col gap-2">
-						<div
+			<UCard
+				variant="soft"
+				class="bg-purple-200"
+			>
+				<div class="h-[250px] flex flex-col gap-4 items-center justify-center">
+					<div class="w-full flex flex-row gap-4 items-center justify-center">
+						<USkeleton
 							class="
 								h-10 max-w-[150px]
 								overflow-x-hidden
 								text-ellipsis
 								whitespace-nowrap
-								bg-gray-200
+								bg-white
 								rounded-lg
 								text-gray-700
 								items-center
-								p-2"
+								p-2
+								shadow-sm"
 						>
 							https://your-very-long-url.com/this-is-a-very-long-url-that-needs-to-be-shortened
-						</div>
-						<div
+						</USkeleton>
+
+						<UButton
+							color="primary"
+							variant="link"
+							icon="i-lucide-arrow-right"
+						/>
+
+						<USkeleton
 							class="
 								h-10
 								bg-primary
@@ -34,11 +45,25 @@
 								text-white
 								items-center
 								p-2
-								w-fit"
+								w-fit
+								shadow-sm"
 						>
 							swft.lk/growth
-						</div>
+						</USkeleton>
 					</div>
+
+					<UCard
+						class="w-[80%]"
+						variant="soft"
+					>
+						<div class="flex items-end justify-start gap-2  h-[70px]">
+							<USkeleton class="h-6 w-1/5 bg-purple-300" />
+							<USkeleton class="h-10 w-1/5 bg-purple-400" />
+							<USkeleton class="h-14 w-1/5 bg-purple-500" />
+							<USkeleton class="h-12 w-1/5 bg-purple-600" />
+							<USkeleton class="h-10 w-1/5 bg-purple-700" />
+						</div>
+					</UCard>
 				</div>
 			</UCard>
 		</template>
