a3482bbd187e673e77d2c520ae606835104721b5
Mak Magz <marktmagcanta.dev@gmail.com>
2026-04-17 21:19:13 +0800

    feat: add hero section #31

---
+++ frontend/app/pages/analytics/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<UPageHero>
+			<template #title>
+				<div class="lg:max-w-3xl mx-auto">
+					Data that drives your brand's <span class="text-primary">growth</span>
+				</div>
+			</template>
+			<template #description>
+				Turn every click into a customer insight.
+				Professional-grade link tracking designed for performance marketing and high-growth teams.
+			</template>
+
+			<template #links>
+				<UButton
+					label="Get Started Free"
+					color="primary"
+					size="xl"
+					class="w-[150px] justify-center"
+				/>
+				<UButton
+					label="Watch Demo"
+					size="xl"
+					variant="outline"
+					class="w-[150px] justify-center"
+				/>
+			</template>
+		</UPageHero>
+	</div>
+</template>
