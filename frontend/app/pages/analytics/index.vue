ed583a42a7005c968d04aac20526c1fa83d3a5a0
Mak Magz <marktmagcanta.dev@gmail.com>
2026-04-17 21:28:15 +0800

    feat: add headline #31

--- frontend/app/pages/analytics/index.vue
+++ frontend/app/pages/analytics/index.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
 		<UPageHero>
+			<template #headline>
+				<UBadge
+					variant="subtle"
+					color="primary"
+					size="sm"
+					class="mb-4 uppercase font-black tracking-wider "
+				>
+					Analytics Cloud 2.0
+				</UBadge>
+			</template>
+
 			<template #title>
 				<div class="lg:max-w-3xl mx-auto">
 					Data that drives your brand's <span class="text-primary">growth</span>
@@ -16,13 +27,13 @@
 					label="Get Started Free"
 					color="primary"
 					size="xl"
-					class="w-[150px] justify-center"
+					class="w-[160px] justify-center text-sm p-3"
 				/>
 				<UButton
 					label="Watch Demo"
 					size="xl"
 					variant="outline"
-					class="w-[150px] justify-center"
+					class="w-[160px] justify-center text-sm p-3"
 				/>
 			</template>
 		</UPageHero>
